1e4664575fede0399431b8b52ca68a1f91570d32
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2021-08-23 19:22:30 +0530

    Commit3 : Creating a helper function which reduces the problem to a smaller sub-problem. The helper function returns the next digit and the carry packed in an object.

---
+++ src/design3/AlienNumberSystem3.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package design3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author PRATAP
+ *
+ */
+public class AlienNumberSystem3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the alien number system (space separated): ");
+		String[] numberSystem = br.readLine().split("\\s+");
+		System.out.println("Enter the number in the above alien number system : ");
+		String inputNumber = br.readLine();
+		String nextNo = findSucc(numberSystem, inputNumber);
+		System.out.println("The next number in the alien number system is : " + nextNo);
+	}
+
+	/*
+	 * Input : base system and the current number. Output : successor number of the
+	 * current number.
+	 */
+	private static String findSucc(String[] numberSystem, String inputNumber) {
+		// TODO Auto-generated method stub
+		/*
+		 * Proper Naming Convention of all the variables and methods for understanding
+		 * easily which variable resembles what. For getting the successor, we need to
+		 * add 1 to the current number and calculate the successor number. So, we need
+		 * to maintain a carry variable which will keep track of the carries. It is
+		 * initialised with 1 because we are also adding 1 to the current number to get
+		 * the next number and hence we can avoid an extra variable.
+		 */
+		int carry = 1;
+		StringBuilder succNumber = new StringBuilder();
+		for (int i = inputNumber.length() - 1; i >= 0; i--) {
+			char currDigit = inputNumber.charAt(i);
+			NextDigitAndCarry next = findNextDigitAnDCarry(currDigit, carry, numberSystem);
+			succNumber.append(numberSystem[next.nextDigit]);
+			carry = next.carry;
+		}
+		if (carry > 0) {
+			succNumber.append(numberSystem[carry]);
+		}
+		return succNumber.reverse().toString();
+	}
+
+	/*
+	 * This helper function takes in the current digit, carry if any and the number
+	 * system and produces the next digit and carry and returns it in a new class
+	 * object.
+	 */
+	public static NextDigitAndCarry findNextDigitAnDCarry(char currDigit, int carry, String[] numberSystem) {
+		int base = numberSystem.length;
+		int number = -1;
+		for (int j = 0; j < numberSystem.length; j++) {
+			if (numberSystem[j].equals(String.valueOf(currDigit))) {
+				number = j;
+				break;
+			}
+		}
+		number += carry;
+		if (number < base)
+			carry = 0;
+		int newDigit = number % base;
+		carry = number / base;
+		return new NextDigitAndCarry(newDigit, carry);
+	}
+}
+
+/*
+ * Object oriented way returning the next digit and the carry when the logic is
+ * moved to a new method in order to breakdown the problem to smaller
+ * subproblems.
+ */
+class NextDigitAndCarry {
+	public int nextDigit;
+	public int carry;
+
+	public NextDigitAndCarry(int nextDigit, int carry) {
+		super();
+		this.nextDigit = nextDigit;
+		this.carry = carry;
+	}
+
+}
